87ccdf5d00835aa77f888c295de1a95430fc107c
Muhammad Saad Asghar <muhammad.asghar@anarock.me>
2024-07-04 17:48:37 +0400

    change position of endpoint

--- routes/api.php
+++ routes/api.php
@@ -35,8 +35,6 @@ Route::group(['prefix' => 'auth'], function($router){
     
 });
 
-Route::post('/get_book_state', [BookController::class, 'get_book_stats']);
-
 Route::group(['middleware' => 'auth:api'], function($router){
 // User Panel Routes Start
 Route::get('/get_subjects', [SubjectController::class, 'get_subjects']);
@@ -48,6 +46,7 @@ Route::post('/add_test', [SettingController::class, 'add_test']);
 Route::get('/get_setting', [SettingController::class, 'get_setting']);
 Route::get('/get_setting_with_units', [SettingController::class, 'get_setting_with_units']);
 Route::post('/save_test_logs', [LogController::class, 'save_test_logs']);
+Route::post('/get_book_state', [BookController::class, 'get_book_stats']);
  // User Panel Routes End
 });  
 
